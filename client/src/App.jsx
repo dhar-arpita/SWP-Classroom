50d823615291eb8e73e71df542b0cac818caee03
dhar-arpita <arpitadhar436@gmail.com>
2026-06-05 23:34:12 +0600

    global axios instance added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,8 @@ import About from './pages/About'
 import PendingEnrollments from './pages/PendingEnrollments'
 import axios from 'axios'
 
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || ''
+
 axios.interceptors.response.use(
   (response) => response,
   (error) => {
