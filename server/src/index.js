4bafcabad2dcf3927ac7a95ad0e5ec17031f2c04
dhar-arpita <arpitadhar436@gmail.com>
2026-06-05 23:24:53 +0600

    port issue resolved

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ import courseRoutes from './routes/courseRoutes.js'
 import enrollmentRoutes from './routes/enrollmentRoutes.js'
 
 const app = express()
-const PORT = process.env.PORT || 5000
+
 
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
@@ -23,8 +23,6 @@ app.use('/api/enrollments', enrollmentRoutes)
 app.get('/api/health', (req, res) => {
   res.json({ message: 'Server চলছে!' })
 })
-
-
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server on ${PORT}`))
