eba4570f599f31affc218ccd462d70f95f7adfac
dhar-arpita <arpitadhar436@gmail.com>
2026-06-11 02:43:10 +0600

    index.js has been changed

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
 const authRoutes = require('./routes/auth')
 const courseRoutes = require('./routes/courses')
 const enrollmentRoutes = require('./routes/enrollments')
